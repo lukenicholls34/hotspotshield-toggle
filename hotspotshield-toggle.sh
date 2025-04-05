7d975804bd7fd459de37c744c68389584496ec6e
lukenicholls34 <40218146+lukenicholls34@users.noreply.github.com>
2025-04-05 22:22:15 +1000

    Added status, toggle, and confirmation functions - toggle function called on execution of script

---
+++ hotspotshield-toggle.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+vpn_status() {
+    vpn_status=$(hotspotshield status | grep "VPN connection state" | awk '{print $5}')
+    echo $vpn_status 
+}
+
+toggle_vpn() {
+    current_vpn_status=$(vpn_status)
+    if [ "$current_vpn_status" == "disconnected" ]
+        then
+        hotspotshield connect
+        confirm_vpn_status_change
+    else
+        hotspotshield disconnect
+        confirm_vpn_status_change
+    fi
+}
+
+confirm_vpn_status_change() {
+    while true
+        do
+        current_vpn_status=$(vpn_status)
+        if [ "$current_vpn_status" == "connected" ] || [ "$current_vpn_status" == "disconnected" ]
+            then
+            echo $current_vpn_status
+            break
+        else
+            sleep 0.25
+        fi
+    done
+}
+
+toggle_vpn
